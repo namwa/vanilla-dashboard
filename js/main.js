6470af734112510c57c065cba1fe692a3a7f34b1
Namwa <sawasdiworn.k@gmail.com>
2024-06-15 12:21:32 +0100

    html, css for nested comment

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-const addNavBtn = document.querySelector(".add-nav")
-const addBlockBtn = document.querySelector(".add-block")
-const columnMain = document.querySelector(".column-main")
+const addNavBtn = document.querySelector('.add-nav')
+const addBlockBtn = document.querySelector('.add-block')
+const columnMain = document.querySelector('.column-main')
 
 // create Nav bar
 function createNavElement() {
-  const nav = document.createElement("nav")
-  const ul = document.createElement("ul")
+  const nav = document.createElement('nav')
+  const ul = document.createElement('ul')
 
-  ul.classList.add("nav")
+  ul.classList.add('nav')
 
-  let menus = ["Logo", "Home", "Blog", "Pricing"]
+  let menus = ['Logo', 'Home', 'Blog', 'Pricing']
 
   menus.forEach((menu, index) => {
-    const li = document.createElement("li")
-    li.setAttribute("draggable", "true")
+    const li = document.createElement('li')
+    li.setAttribute('draggable', 'true')
 
     li.innerText = menu
 
     if (index === 0) {
-      li.classList.add("logo")
+      li.classList.add('logo')
     }
 
-    li.addEventListener("dragstart", handleDragStart)
-    li.addEventListener("dragover", handleDragOver)
-    li.addEventListener("dragenter",handleDragEnter)
-    li.addEventListener("dragleave", handleDragLeave)
-    li.addEventListener("drop", handleDrop)
-    li.addEventListener("dragend", handleDragEnd)
+    li.addEventListener('dragstart', handleDragStart)
+    li.addEventListener('dragover', handleDragOver)
+    li.addEventListener('dragenter',handleDragEnter)
+    li.addEventListener('dragleave', handleDragLeave)
+    li.addEventListener('drop', handleDrop)
+    li.addEventListener('dragend', handleDragEnd)
 
     ul.appendChild(li)
   })
@@ -37,64 +37,69 @@ function createNavElement() {
 }
 
 function handleDragStart(e) {
-  e.target.classList.add("dragging")
-  e.dataTransfer.effectAllowed = "move"
-  e.dataTransfer.setData("text/html", e.target.innerHTML)
+  e.target.classList.add('dragging')
+  e.dataTransfer.effectAllowed = 'move'
+  e.dataTransfer.setData('text/html', e.target.innerHTML)
 
-  e.currentTarget.style.backgroundColor = "yellow"
+  e.currentTarget.style.backgroundColor = 'yellow'
 }
 
 function handleDragOver(e) {
   e.preventDefault();
-  e.dataTransfer.dropEffect = "move"
+  e.dataTransfer.dropEffect = 'move'
   return false
 }
 
 function handleDragEnter(e) {
-  this.classList.add("over")
+  this.classList.add('over')
 }
 
 function handleDragLeave(e) {
-  this.classList.remove("over")
+  this.classList.remove('over')
 }
 
 function handleDrop(e) {
   e.stopPropagation()
-  const draggingItem = document.querySelector(".dragging")
+  const draggingItem = document.querySelector('.dragging')
   if (draggingItem !== this) {
     draggingItem.innerHTML = this.innerHTML
-    this.innerHTML = e.dataTransfer.getData("text/html")
+    this.innerHTML = e.dataTransfer.getData('text/html')
   }
   return false
 }
 
 function handleDragEnd(e) {
-  this.classList.remove("dragging")
-  e.currentTarget.style.backgroundColor = ""
+  this.classList.remove('dragging')
+  e.currentTarget.style.backgroundColor = ''
 
-  let menus = document.getElementsByTagName("li")
+  let menus = document.getElementsByTagName('li')
   for (menu of menus) {
-    menu.classList.remove("over")
+    menu.classList.remove('over')
   }
 }
 
 // create a block
 function createBlockElement() {
-  const block = document.createElement("div")
-  const textarea = document.createElement("textarea")
+  const block = document.createElement('div')
+  const textarea = document.createElement('textarea')
 
-  block.classList.add("block")
-  textarea.setAttribute("placeholder", "Enter you text here")
+  block.classList.add('block')
+  textarea.setAttribute('placeholder', 'Enter you text here')
   block.appendChild(textarea)
   return block
 }
 
-addNavBtn.addEventListener("click", () => {
+addNavBtn.addEventListener('click', () => {
   const nav = createNavElement()
   columnMain.appendChild(nav)
 })
 
-addBlockBtn.addEventListener("click", () => {
+addBlockBtn.addEventListener('click', () => {
   const block = createBlockElement()
   columnMain.appendChild(block)
 })
+
+// comments
+function createCommentBox() {
+  document.createElement('div')
+}
